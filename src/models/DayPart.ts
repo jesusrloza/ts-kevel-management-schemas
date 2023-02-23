d64bb04d2e2e1f9f039ad0db5bca6db6028e0c33
Jesús R. Loza <jesusr.loza@gmail.com>
2023-02-23 13:23:46 -0600

    corrections in DayPart for it to be able to parse get/list day part results

--- src/models/DayPart.ts
+++ src/models/DayPart.ts
@@ -5,9 +5,11 @@ const timeSchema = z.string().regex(/^([01]\d|2[0-3]):([0-5]\d):([0-5]\d)$/)
 
 export const DayPartSchema = z.object({
   TimepartID: z.number().optional(),
+  Id: z.number().optional(),
   StartTime: timeSchema,
   EndTime: timeSchema,
   WeekDays: z.array(DayOfTheWeekEnum),
+  FlightId: z.number().optional(),
 })
 
 export type DayPartSchema = z.infer<typeof DayPartSchema>
