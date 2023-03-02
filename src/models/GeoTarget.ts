d46cdeeaa3731cb2a7762d76c5d6ef072252dec8
Jesús R. Loza <jesusr.loza@gmail.com>
2023-03-02 13:59:23 -0600

    changes on DateSchema to include offset

--- src/models/GeoTarget.ts
+++ src/models/GeoTarget.ts
@@ -1,11 +1,12 @@
 import { z } from 'zod'
 
 export const GeoTargetSchema = z.object({
+  LocationId: z.number().optional().nullable(),
+  FlightId: z.number().optional().nullable(),
   CountryCode: z.string().optional(),
-  Region: z.string().optional(),
+  Region: z.string().optional().nullable(),
   MetroCode: z.number().optional(),
   IsExclude: z.boolean().default(false).nullable(),
-  LocationId: z.number().optional().nullable(),
 })
 
 export type GeoTargetSchema = z.infer<typeof GeoTargetSchema>
--- src/models/base/Date.ts
+++ src/models/base/Date.ts
@@ -1,11 +1,17 @@
 import { z } from 'zod'
 
-export const DateSchema = z.string().refine((value) => {
-  const dateRegex = /^\d{4}-\d{2}-\d{2}$/
-  const dateTimeRegex = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(.\d{3})?Z?$/
-  const kevelTimeRegex = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(.\d{7})?$/
+function isDateString(value: string): boolean {
+  const date = /^\d{4}-\d{2}-\d{2}$/
+  const dateTime = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(.\d{3})?Z?$/
+  const kevelTime = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(.\d{7})?([+-]\d{2}:\d{2})?$/
 
-  return dateRegex.test(value) || dateTimeRegex.test(value) || kevelTimeRegex.test(value)
-}, 'Expected a date or date-time string in the format YYYY-MM-DD or YYYY-MM-DDTHH:mm:ss.sssZ or YYYY-MM-DDTHH:mm:ss.sssssss')
+  return date.test(value) || dateTime.test(value) || kevelTime.test(value)
+}
 
+export const DateSchema = z
+  .string()
+  .refine(
+    isDateString,
+    'Expected a date or date-time string in the format YYYY-MM-DD or YYYY-MM-DDTHH:mm:ss.sssZ or YYYY-MM-DDTHH:mm:ss.sssssss'
+  )
 export type DateSchema = z.infer<typeof DateSchema>
