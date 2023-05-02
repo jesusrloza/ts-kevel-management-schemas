3321ad59b845d2e4c95d9d4dff2ce144a47a46f2
Jesús R. Loza <jesusr.loza@gmail.com>
2023-05-02 17:28:06 -0600

    Changed Campaign and Flight Schemas. Documentation omitted some properties from their response

--- src/models/Campaign.ts
+++ src/models/Campaign.ts
@@ -1,30 +1,33 @@
-import { z } from 'zod'
-import { FlightSchema } from './Flight'
 import { CapTypeEnum, FrequencyCapTypeEnum } from './base/enums'
 import { DateSchema } from './base/Date'
+import { FlightSchema } from './Flight'
+import { z } from 'zod'
 
 export const CampaignSchema = z.object({
-  Id: z.number().optional(),
   AdvertiserId: z.number(),
-  Name: z.string(),
-  StartDate: z.string().optional(),
+  CapType: CapTypeEnum.default(4).nullable(),
+  Created: DateSchema.optional().nullable(),
+  CustomFieldsJson: z.string().optional().nullable(),
+  DailyCapAmount: z.number().optional().nullable(),
+  DontAffectParentFreqCap: z.boolean().optional().nullable(),
+  EndDate: z.string().optional().nullable(),
+  EndDateISO: DateSchema.optional().nullable(),
   Flights: z.array(FlightSchema).default([]),
-  IsActive: z.boolean().default(false),
-  IsDeleted: z.boolean().optional(),
-  IsArchived: z.boolean().optional(),
   FreqCap: z.number().optional().nullable(),
   FreqCapDuration: z.number().optional().nullable(),
   FreqCapType: FrequencyCapTypeEnum.optional().nullable(),
-  DontAffectParentFreqCap: z.boolean().optional().nullable(),
-  CapType: CapTypeEnum.default(4).nullable(),
-  DailyCapAmount: z.number().optional().nullable(),
-  LifetimeCapAmount: z.number().optional().nullable(),
-  CustomFieldsJson: z.string().optional().nullable(),
-  EndDate: z.string().optional().nullable(),
+  Id: z.number().optional(),
+  IsActive: z.boolean().default(false),
+  IsArchived: z.boolean().optional(),
+  IsDeleted: z.boolean().optional(),
   IsFreqCap: z.boolean().optional().nullable(),
-  EndDateISO: DateSchema.optional().nullable(),
+  LastModified: DateSchema.optional().nullable(),
+  LifetimeCapAmount: z.number().optional().nullable(),
+  Name: z.string(),
   Price: z.number().optional().nullable(),
+  StartDate: z.string().optional(),
   StartDateISO: DateSchema.optional().nullable(),
+  Version: z.number().optional().nullable(),
 })
 
 export type CampaignSchema = z.infer<typeof CampaignSchema>
